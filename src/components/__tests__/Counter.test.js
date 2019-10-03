864ac635e45d0be6786bf9e4faf37e34fc19f4b2
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-03 18:00:27 +0100

    added test to counter component

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -5,9 +5,12 @@ import Counter from '../Counter';
 
 let tools;
 
+let countLimit = 5;
+
+
 beforeEach(() => {
   rtl.cleanup();
-  tools = rtl.render(<Counter user='Peter' />);
+  tools = rtl.render(<Counter user='Peter' countLimit={countLimit} />);
 });
 
 describe('Counter component', () => {
@@ -39,34 +42,63 @@ describe('Counter component', () => {
 
   it('can decrement the count by one by clicking decrement', () => {
     // implement
-    const decButton = tools.queryByTestId('incButton');
+    const decButton = tools.queryByTestId('decButton');
 
     rtl.fireEvent.click(decButton);
     expect(tools.queryByText(/0/)).not.toBeInTheDocument();
     expect(tools.queryByText(/-1/)).toBeInTheDocument();
 
-    rtl.fireEvent.click(decButton);
-    expect(tools.queryByText(/-1/)).not.toBeInTheDocument();
-    expect(tools.queryByText(/-2/)).toBeInTheDocument();
   });
 
   it('can reset the count clicking rest', () => {
     // implement
+    const resetButton = tools.queryByTestId('resetButton');
+
+    rtl.fireEvent.click(resetButton);
+    expect(tools.queryByText(/0/)).toBeInTheDocument();
   });
 
   it('prevents the count from going over an upper limit', () => {
     // implement
+    const incButton = tools.queryByTestId('incButton');
+    for(let i = 0; i < countLimit+1; i++ ) {
+      rtl.fireEvent.click(incButton);
+    }
+    expect(tools.queryByText(new RegExp(countLimit, 'i'))).toBeInTheDocument();
+
   });
 
   it('prevents the count from going under a lower limit', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+    for(let i = 0; i > -countLimit-1; i-- ) {
+      rtl.fireEvent.click(decButton);
+    }
+    expect(tools.queryByText(new RegExp(-countLimit, 'i'))).toBeInTheDocument();
+
   });
 
   it('shows a warning once we hit the upper limit of the counter', () => {
     // implement
+
+    const incButton = tools.queryByTestId('incButton');
+    for(let i = 0; i < countLimit; i++ ) {
+      rtl.fireEvent.click(incButton);
+    }
+    expect(tools.queryByText(new RegExp('as high as', 'ig'))).toBeInTheDocument();
+    expect(tools.queryByText(new RegExp(countLimit))).toBeInTheDocument();
+    // count === countLimit &&
+    // <h5 style={{ color: 'red' }}>That's as high as {user}'s count will go</h5>
+    
   });
 
   it('shows a warning once we hit the lower limit of the counter', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+    for(let i = 0; i > -countLimit; i-- ) {
+      rtl.fireEvent.click(decButton);
+    }
+    expect(tools.queryByText(new RegExp(-countLimit))).toBeInTheDocument();
+    expect(tools.queryByText(new RegExp('as low as', 'ig'))).toBeInTheDocument();
   });
 });
